3d3e929e75fc33f6e006c4978bc0cc441fb0db8a
Maria Clemmensen <clemmensen.maria@gmail.com>
2021-04-12 11:57:44 +0200

    Base loginController
    Pre usage of DLL

---
+++ RegnskabsSystem/RegnskabsSystem/RegnskabsSystem/Controllers/ApiController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RegnskabsSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegnskabsSystem.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [HttpPost("user/login")]
+        public ActionResult<TokenModel> Login([FromBody] string user, [FromBody] string password)
+        {
+            // Use DLL from Kennie later by sending user and password in
+            var tokenFetched = new Guid();
+            var token = new TokenModel();
+            token.User = user;
+            token.Token = tokenFetched.ToString();
+            return token;
+        }
+    }
+}
