9979ca04fc9a71a65e713817279cdd52baa50abc
Gutzon <kenniegroth@gmail.com>
2021-04-12 10:50:47 +0200

    Started on interface

---
+++ RegnskabsSystem/RegnskabsSystem/ServerSideData/ServerSideData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using ServerSideData.Models;
+
+namespace ServerSideData
+{
+    public class ServerSideData : IServerSideData
+    {
+        public bool CreateMember(string tokken, Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateUser(string tokken, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteMember(string tokken, Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteUser(string tokken, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditMember(string tokken, Member member)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditUser(string tokken, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FinansEntry> GetFinans(string tokken, string konti, string searchvalue = "", string searchtype = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Permissions GetPermissions(string tokken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Logout(string tokken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
