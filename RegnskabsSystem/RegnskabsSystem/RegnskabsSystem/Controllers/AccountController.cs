b8a1f83ec1e4f5acb0aee507b2f503771b10023a
Maria Clemmensen <clemmensen.maria@gmail.com>
2021-04-15 11:14:20 +0200

    Prepare account base overview

---
+++ RegnskabsSystem/RegnskabsSystem/RegnskabsSystem/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegnskabsSystem.Controllers
+{
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet("view")]
+        public ActionResult AccountView()
+        {
+            return View();
+        }
+        
+    }
+}
