6cd003ee822e74e2a2aba2d132ceecf784a0444e
Maria Clemmensen <clemmensen.maria@gmail.com>
2021-04-22 20:53:56 +0200

    Match expected params

--- RegnskabsSystem/RegnskabsSystem/RegnskabsSystem/wwwroot/js/helper.js
+++ RegnskabsSystem/RegnskabsSystem/RegnskabsSystem/wwwroot/js/helper.js
@@ -246,7 +246,7 @@ function showButton(elmId, givesRights) {
  */
 function hasPermission(givesRights) {
     let ownPermissions = getPermissions();
-    let hasRights = (ownPermissions.addCorporation || ownPermissions.admin || hasAMatchingRight(givesRights));
+    let hasRights = (ownPermissions.addCorporation || ownPermissions.admin || hasAMatchingRight(ownPermissions, ownPermissionsgivesRights));
     return hasRights;
 }
 
